b85c9fb4cd762f7957ad8ff4d60653936a5aa1f3
German Alamilla Peralta <german.alamilla@gmail.com>
2025-05-01 14:23:52 -0600

    section changes

--- components/text-parallax-content.tsx
+++ components/text-parallax-content.tsx
@@ -22,13 +22,13 @@ export const TextParallaxContentExample = () => {
       >
         <ExampleContent />
       </TextParallaxContent>
-      <TextParallaxContent
+      {/* <TextParallaxContent
         imgUrl="https://images.unsplash.com/photo-1504610926078-a1611febcad3?q=80&w=2416&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         subheading="Modern"
         heading="Dress for the best."
       >
         <ExampleContent />
-      </TextParallaxContent>
+      </TextParallaxContent> */}
     </div>
   );
 };
