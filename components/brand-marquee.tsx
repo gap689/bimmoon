981313050d464293e201e55c17676fcf9969cb9c
German Alamilla Peralta <german.alamilla@gmail.com>
2025-05-01 14:17:03 -0600

    logos change

--- components/brand-marquee.tsx
+++ components/brand-marquee.tsx
@@ -10,16 +10,15 @@ const brands = [
   { name: "redbull", src: "/brands/redbull.svg" },
   { name: "CocaCola", src: "/brands/coca-cola.svg" },
   { name: "UPS", src: "/brands/ups.svg" },
-  { name: "Nike", src: "/brands/nike.svg" },
-  { name: "Amazon", src: "/brands/amazon.svg" },
-  { name: "Ikea", src: "/brands/ikea.svg" },
-  { name: "Puma", src: "/brands/puma.svg" },
+  { name: "Yamaha", src: "/brands/yamaha.svg" },
+  { name: "Ebay", src: "/brands/ebay.svg" },
+  { name: "Levis", src: "/brands/levis.svg" },
 ];
 
 export function BrandMarquee() {
   return (
-    <section className="relative w-full overflow-hidden py-6 gap-8 border-t border-b bg-current">
-      <Marquee pauseOnHover speed={50} gradient>
+    <section className="relative w-full overflow-hidden py-6 border-t border-b bg-transparent">
+      <Marquee pauseOnHover speed={50}>
         { brands.map((brand, i) => (
           <div
             key={i}
