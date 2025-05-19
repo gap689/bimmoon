426eb09e8eeb7522775a4abd7fa3ea2a5934f28f
German Alamilla Peralta <german.alamilla@gmail.com>
2025-05-18 22:41:30 -0600

    about page v1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 import { BrandMarquee } from "@/components/brand-marquee";
 import { Vortex } from "@/components/ui/vortex";
+import WhatsAppButton from "@/components/whatsapp-button";
 
 const AboutPage = () => {
   return ( 
@@ -18,8 +19,8 @@ const AboutPage = () => {
           </p>
           <div>
             <h2 className="uppercase font-medium text-xs my-4 text-[rgb(18,87,246)]">what we do</h2>
-            <p className="text-neutral-400 font-medium font-sm">
-              En Bimmoon, transformamos ideas en <span className="text-[rgb(18,87,246)]">productos únicos</span> que marcan la diferencia. Diseñamos y fabricamos <span className="text-[rgb(18,87,246)]">letreros</span>, <span className="text-[rgb(18,87,246)]">llaveros</span>, <span className="text-[rgb(18,87,246)]">portaplacas</span>, <span className="text-[rgb(18,87,246)]">calcomanías</span> y más, personalizados con <span className="text-[rgb(18,87,246)]">materiales de alta calidad</span> y precisión de <span className="text-[rgb(18,87,246)] font-semibold">corte láser</span>. Nos enfocamos en cada detalle para que cada pieza refleje tu estilo, marca o mensaje con originalidad y profesionalismo. Ya sea para un negocio, un regalo o uso personal, ofrecemos soluciones creativas, duraderas y <span className="text-[rgb(18,87,246)]">a la medida</span> de tus necesidades. Si puedes imaginarlo, podemos crearlo.
+            <p className="text-neutral-300 font-medium font-sm md:font-base">
+              En Bimmoon, transformamos ideas en <span className="text-[rgb(18,87,246)]">productos únicos</span> que marcan la diferencia. Diseñamos y fabricamos <span className="text-[rgb(18,87,246)]">letreros</span>, <span className="text-[rgb(18,87,246)]">llaveros</span>, <span className="text-[rgb(18,87,246)]">portaplacas</span>, <span className="text-[rgb(18,87,246)]">calcomanías</span> y más, personalizados con <span className="text-[rgb(18,87,246)]">materiales de alta calidad</span> y precisión de <span className="text-[rgb(18,87,246)] font-semibold">corte láser</span>. Nos enfocamos en cada detalle para que cada pieza refleje tu estilo, marca o mensaje con originalidad y profesionalismo. Ya sea para un negocio, un regalo o uso personal, ofrecemos soluciones creativas, duraderas y <span className="text-[rgb(18,87,246)]">a la medida</span> de tus necesidades. <span className="text-white font-semibold">Si puedes imaginarlo, podemos crearlo.</span>
             </p>
           </div>
 
@@ -27,8 +28,14 @@ const AboutPage = () => {
             <h2 className="uppercase font-medium text-[rgb(18,87,246)] my-4 text-xs">Subtítulo</h2>
             <BrandMarquee/>
           </div>
+          <div className="mt-10">
+            <h2 className="uppercase font-medium text-[rgb(18,87,246)] my-4 text-xs">Contáctanos</h2>
+            <WhatsAppButton/>
+            <div className="py-2">
+              email: rotulos.bimmoon@gmail.com
+            </div>
+          </div>
         </div>
-
       </Vortex>
     </div>
   );
