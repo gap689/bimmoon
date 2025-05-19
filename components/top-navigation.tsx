29bc2b9d2f77c43338a33d8b4332e0f658ca4c37
German Alamilla Peralta <german.alamilla@gmail.com>
2025-05-18 21:13:25 -0600

    changed top navigation padding

--- components/top-navigation.tsx
+++ components/top-navigation.tsx
@@ -5,7 +5,7 @@ import { NavMenu } from "@/components/nav-menu";
 export function TopNavigation() {
   return (
     <header className="w-full backdrop-blur-sm fixed top-0 left-0 right-0 z-30">
-      <nav className="relative flex items-center justify-between h-16 mx-4">
+      <nav className="relative flex items-center justify-between h-16 px-2 sm:px-4 lg:px-6">
 
         <a href="/" className="flex items-center justify-start shrink-0">
           {/* Optional: Nav buttons or hamburger */}
