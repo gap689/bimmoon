ee36972e1fb7f7dcf48d9660846a32c41200a410
German Alamilla Peralta <german.alamilla@gmail.com>
2025-05-20 23:54:24 -0600

    portaplacas page

---
+++ app/productos/portaplacas/page.tsx
@@ -0,0 +1,9 @@
+const PortaplacasPage = () => {
+  return ( 
+    <div className="h-full flex items-center justify-center">
+      Portaplacas
+    </div>
+  );
+}
+ 
+export default PortaplacasPage;
