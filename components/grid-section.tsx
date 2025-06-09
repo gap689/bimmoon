1684caacc0d456af908196aaad6848f10d94eb6b
German Alamilla Peralta <german.alamilla@gmail.com>
2025-06-08 20:48:28 -0600

    components/grid-section.tsx

--- components/grid-section.tsx
+++ components/grid-section.tsx
@@ -17,7 +17,7 @@ const GridSection = () => {
 
           <div className="absolute inset-0 bg-black/20" />
     
-          <a href="/productos/cortelaser" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
+          <a href="/productos/corteacrilico" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
             Cortes y grabados con laser CO2
           </a>
         </div>
@@ -68,6 +68,23 @@ const GridSection = () => {
           </a>
         </div> 
         <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
+          <video
+            className="w-full h-full object-cover"
+            autoPlay
+            muted
+            loop
+            playsInline
+          >
+            <source src="/videos/estampados.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+
+          <div className="absolute inset-0 bg-black/20" />
+          <a href="/productos/estampadoseimpresiones" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
+            Estampados
+          </a>
+        </div> 
+        <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
           <Image
             src="/images/rotulos.jpeg"
             alt={`rotulos`}
@@ -89,6 +106,18 @@ const GridSection = () => {
           <div className="absolute inset-0 bg-black/20" />
           <h2 className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
             Estampados
+          </h2>
+        </div> 
+        <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
+          <Image
+            src="/images/emblemas.jpg"
+            alt={`emblemas`}
+            fill
+            className="object-cover"
+          />
+          <div className="absolute inset-0 bg-black/20" />
+          <h2 className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
+            Emblemas
           </h2>
         </div> 
     </div>
