1d11ec5ca5367d6201711be95fa926ff31d102c6
German Alamilla Peralta <german.alamilla@gmail.com>
2025-06-08 21:36:53 -0600

    added sections

--- components/grid-section.tsx
+++ components/grid-section.tsx
@@ -1,125 +1,137 @@
 import Image from "next/image";
 
 const GridSection = () => {
-  return ( 
-    <div className="h-full grid grid-cols-2">
-        <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
-          <video
-            className="w-full h-full object-cover"
-            autoPlay
-            muted
-            loop
-            playsInline
-          >
-            <source src="/lasercut.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+  return (
+    <div className="grid grid-cols-2">
+      <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
+        <video
+          className="w-full h-full object-cover"
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source src="/lasercut.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
 
-          <div className="absolute inset-0 bg-black/20" />
-    
-          <a href="/productos/corteacrilico" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
-            Cortes y grabados con laser CO2
-          </a>
-        </div>
-        <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
-          <Image
-            src="/stickers.jpeg"
-            alt={`stickers section`}
-            fill
-            className="object-cover"
-          />
-          <div className="absolute inset-0 bg-black/20" />
-          <a href="/productos/stickers" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
-            Stickers
-          </a>
-        </div>
-        <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
-          <video
-            className="w-full h-full object-cover"
-            autoPlay
-            muted
-            loop
-            playsInline
-          >
-            <source src="/cutting.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+        <div className="absolute inset-0 bg-black/20" />
+  
+        <a href="/productos/corteacrilico" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
+          Cortes y grabados con laser CO2
+        </a>
+      </div>
+      <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
+        <Image
+          src="/stickers.jpeg"
+          alt={`stickers section`}
+          fill
+          className="object-cover"
+        />
+        <div className="absolute inset-0 bg-black/20" />
+        <a href="/productos/stickers" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
+          Stickers
+        </a>
+      </div>
+      <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
+        <video
+          className="w-full h-full object-cover"
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source src="/cutting.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
 
-          <div className="absolute inset-0 bg-black/20" />
-          <a href="/productos/letreros" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
-            Letreros
-          </a>
-        </div>
-        <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
-          <video
-            className="w-full h-full object-cover"
-            autoPlay
-            muted
-            loop
-            playsInline
-          >
-            <source src="/portaplacas.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+        <div className="absolute inset-0 bg-black/20" />
+        <a href="/productos/letreros" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
+          Letreros
+        </a>
+      </div>
+      <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
+        <video
+          className="w-full h-full object-cover"
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source src="/portaplacas.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
 
-          <div className="absolute inset-0 bg-black/20" />
-          <a href="/productos/portaplacas" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
-            Portaplacas
-          </a>
-        </div> 
-        <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
-          <video
-            className="w-full h-full object-cover"
-            autoPlay
-            muted
-            loop
-            playsInline
-          >
-            <source src="/videos/estampados.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+        <div className="absolute inset-0 bg-black/20" />
+        <a href="/productos/portaplacas" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
+          Portaplacas
+        </a>
+      </div> 
+      <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
+        <video
+          className="w-full h-full object-cover"
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source src="/videos/estampados.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
 
-          <div className="absolute inset-0 bg-black/20" />
-          <a href="/productos/estampadoseimpresiones" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
-            Estampados
-          </a>
-        </div> 
-        <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
-          <Image
-            src="/images/rotulos.jpeg"
-            alt={`rotulos`}
-            fill
-            className="object-cover"
-          />
-          <div className="absolute inset-0 bg-black/20" />
-          <a href="/productos/rotulos" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
-            Rótulos
-          </a>
-        </div> 
-        <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
-          <Image
-            src="/images/estampados.jpeg"
-            alt={`estampados`}
-            fill
-            className="object-cover"
-          />
-          <div className="absolute inset-0 bg-black/20" />
-          <h2 className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
-            Estampados
-          </h2>
-        </div> 
-        <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
-          <Image
-            src="/images/emblemas.jpg"
-            alt={`emblemas`}
-            fill
-            className="object-cover"
-          />
-          <div className="absolute inset-0 bg-black/20" />
-          <h2 className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
-            Emblemas
-          </h2>
-        </div> 
+        <div className="absolute inset-0 bg-black/20" />
+        <a href="/productos/estampadoseimpresiones" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
+          Estampados
+        </a>
+      </div> 
+      <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
+        <Image
+          src="/images/rotulos.jpeg"
+          alt={`rotulos`}
+          fill
+          className="object-cover"
+        />
+        <div className="absolute inset-0 bg-black/20" />
+        <a href="/productos/rotulos" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
+          Rótulos
+        </a>
+      </div> 
+      <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
+        <Image
+          src="/images/estampados.jpeg"
+          alt={`estampados`}
+          fill
+          className="object-cover"
+        />
+        <div className="absolute inset-0 bg-black/20" />
+        <h2 className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
+          Estampados
+        </h2>
+      </div> 
+      <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
+        <Image
+          src="/images/emblemas.jpg"
+          alt={`emblemas`}
+          fill
+          className="object-cover"
+        />
+        <div className="absolute inset-0 bg-black/20" />
+        <h2 className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
+          Emblemas
+        </h2>
+      </div> 
+      <div className="relative h-60 md:h-64 xl:h-72 col-span-2 md:col-span-1">
+        <Image
+          src="/images/marimbas.jpg"
+          alt={`rotulos`}
+          fill
+          className="object-cover"
+        />
+        <div className="absolute inset-0 bg-black/20" />
+        <a href="/productos" className="absolute bottom-2 right-4 font-semibold bg-black/70 shrink-0 rounded-full px-4 py-2 sm:text-xs text-[10px] font-michroma">
+          Más...
+        </a>
+      </div> 
     </div>
   );
 }
