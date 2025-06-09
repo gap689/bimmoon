60d5e2d2472dc4809f443230ffec225667f9fc04
German Alamilla Peralta <german.alamilla@gmail.com>
2025-06-08 20:46:34 -0600

    changed letter size for larger screens

--- components/section.tsx
+++ components/section.tsx
@@ -7,10 +7,10 @@ const Section = () => {
       <div className="text-4xl md:text-5xl uppercase font-semibold py-4">
         BIMMOON
       </div>
-      <div className="max-w-xl font-bold font-michroma uppercase text-sm mb-5">
+      <div className="max-w-xl font-bold font-michroma uppercase text-sm lg:font-base mb-5">
         Nos especializamos en diseñar y personalizar productos únicos para ti. Ofrecemos letreros, portaplacas, llaveros, espejos y mucho más, todo hecho con materiales de alta calidad y tecnología de corte láser. 
       </div>
-      <div className="max-w-xl mb-5 text-sm">
+      <div className="max-w-xl mb-5 text-sm lg:text-base">
       Cada pieza es elaborada con atención al detalle y pasión por el diseño, creando algo que realmente represente tu estilo y personalidad.
       Ya sea que busques algo para tu hogar, tu negocio, tu carro o para un regalo especial, tenemos lo que necesitas. Cada producto está hecho a mano y se puede personalizar para que encaje perfectamente con lo que imaginas.
       </div>
