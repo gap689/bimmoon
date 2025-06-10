88a1f35e7faaaa964e1d01d72feee627ec09c5f4
German Alamilla Peralta <german.alamilla@gmail.com>
2025-06-10 13:10:27 -0600

    text size change

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -5,7 +5,7 @@ export const BackButton = () => {
   return (
     <Link scroll={true}
       href="/productos"
-      className="px-4 py-2 hover:text-yellow-300 flex items-center gap-2 cursor-pointer"
+      className="px-4 py-2 hover:text-yellow-300 flex items-center gap-2 cursor-pointer text-sm"
     >
       <ChevronLeft className='w-5 h-5 shrink-0'/> Volver
     </Link>
