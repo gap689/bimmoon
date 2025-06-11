a69ce92d91c4b946d04a433eb63acae41520b613
German Alamilla Peralta <german.alamilla@gmail.com>
2025-06-11 13:20:24 -0600

    added label prop to button

--- components/whatsapp-button.tsx
+++ components/whatsapp-button.tsx
@@ -1,6 +1,10 @@
 import { FaWhatsapp } from "react-icons/fa";
 
-const WhatsAppButton = () => {
+interface WhatsAppButtonProps {
+  label?: string;
+}
+
+const WhatsAppButton = ({ label = "¡Pide por Whatsapp!" }: WhatsAppButtonProps) => {
   return ( 
     <a
       href="https://wa.me/526646704470?text=Hola%2C%20quiero%20cotizar%20un%20producto%20personalizado.%20%C2%BFMe%20puedes%20ayudar%3F"
@@ -10,7 +14,9 @@ const WhatsAppButton = () => {
       className="flex items-center w-fit gap-2 px-4 py-2 bg-green-500 text-white rounded-full hover:bg-green-600 transition"
     >
       <FaWhatsapp className="w-4 h-4 shrink-0" />
-      <span className="hidden sm:inline text-xs text-nowrap font-michroma">¡Pide por Whatsapp!</span>
+      <span className="hidden sm:inline text-xs text-nowrap font-michroma">
+        {label}
+      </span>
     </a>
   );
 }
